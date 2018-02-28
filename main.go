87e9104f08b9fafa76259c23af82bbfa6cd0cd09
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-02-28 17:10:13 +0900

    add cleaner for GOCACHE

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 var (
-	cleaners = []*cleanerInfo{}
-	dryrun   = flag.Bool("dryrun", true, "dryrun")
-	verbose  = flag.Bool("verbose", true, "verbose")
+	cis     = []*cleanerInfo{}
+	dryrun  = flag.Bool("dryrun", true, "dryrun")
+	verbose = flag.Bool("verbose", true, "verbose")
+	cleaner = flag.String("cleaner", "*", "specify cleaners (comma separated, ? for list)")
 )
 
 type cleanerInfo struct {
@@ -20,13 +22,35 @@ type cleanerInfo struct {
 }
 
 func register(name string, cleaner func(bool, bool) (string, error)) {
-	cleaners = append(cleaners, &cleanerInfo{name: name, cleaner: cleaner})
+	cis = append(cis, &cleanerInfo{name: name, cleaner: cleaner})
+}
+
+func willdo(ci *cleanerInfo) bool {
+	if *cleaner == "*" {
+		return true
+	}
+	for _, name := range strings.Split(*cleaner, ",") {
+		if name == ci.name {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	flag.Parse()
 
-	for _, c := range cleaners {
+	if *cleaner == "?" {
+		for _, ci := range cis {
+			fmt.Println(ci.name)
+		}
+		os.Exit(1)
+	}
+
+	for _, c := range cis {
+		if !willdo(c) {
+			continue
+		}
 		result, err := c.cleaner(*dryrun, *verbose)
 		if err != nil {
 			log.Printf("%s: %s\n", c.name, err)
@@ -34,7 +58,7 @@ func main() {
 		}
 		c.result = result
 	}
-	for _, c := range cleaners {
+	for _, c := range cis {
 		if c.result != "" {
 			fmt.Printf("%s: %s\n", c.name, c.result)
 		}
--- tmpdir.go
+++ tmpdir.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -27,6 +28,7 @@ var (
 
 func init() {
 	register("TmpDir", cleanTmpDir)
+	register("CacheDir", cleanCacheDir)
 }
 
 func dirsize(name string) uint64 {
@@ -50,9 +52,62 @@ func dirsize(name string) uint64 {
 	return size
 }
 
+func cleanCacheDir(dryrun, verbose bool) (string, error) {
+	b, err := exec.Command("go", "env", "GOCACHE").CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	name := strings.TrimSpace(string(b))
+	if name == "" {
+		return "", os.ErrNotExist
+	}
+	dir, err := os.Open(name)
+	if err != nil {
+		return "", err
+	}
+	defer dir.Close()
+
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		return "", err
+	}
+	var size uint64
+	for _, fi := range fis {
+		p := filepath.Join(name, fi.Name())
+		if verbose {
+			log.Println("CacheDir:", p)
+		}
+		if fi.IsDir() {
+			size += dirsize(p)
+			if !dryrun {
+				err = os.RemoveAll(p)
+				if err != nil {
+					return "", err
+				}
+			}
+		} else {
+			size += uint64(fi.Size())
+			if !dryrun {
+				err = os.Remove(p)
+				if err != nil {
+					return "", err
+				}
+			}
+		}
+	}
+	result := "%.1f MB removed"
+	if dryrun {
+		result = "%.1f MB removable"
+	}
+	return fmt.Sprintf(result, float64(size)/1024/1024), nil
+}
+
 func cleanTmpDir(dryrun, verbose bool) (string, error) {
 	name := os.TempDir()
 	dir, err := os.Open(name)
+	if err != nil {
+		return "", err
+	}
 	defer dir.Close()
 
 	fis, err := dir.Readdir(-1)
